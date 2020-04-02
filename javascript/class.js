226e4924200c459df37defc5390e441a2d66622f
shubhampal39@gmail.com <shubhampal39@gmail.com>
2020-04-02 07:45:43 +0530

    javascript updated

--- javascript/class.js
+++ javascript/class.js
@@ -7,10 +7,17 @@ let x={
 		p:"Add",
 		q:true
 	}
-}
+	anumber=12345;
+	astring="abscde";
+	afunction:function(){
+		return (this.anumber+this.astring)
+		
+		}
 
+}
 x.k=[1,2]
 console.log(x)
 console.log(x.b)
 
 console.log(x.e.p)
+console.log(x.afunction())
